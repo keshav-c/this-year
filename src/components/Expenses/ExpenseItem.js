4db6494f8ec1b690704368fdd580d876e5d4e1c5
Keshav Chakravarthy <keshav1985@gmail.com>
2021-06-13 17:23:11 +0530

    Add button with handler to ExpenseItem.

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,22 @@
-import './ExpenseItem.css'
-import Card from '../UI/Card';
-import ExpenseDate from './ExpenseDate'
+import "./ExpenseItem.css";
+import Card from "../UI/Card";
+import ExpenseDate from "./ExpenseDate";
 
-const ExpenseItem = (props) => (
+const ExpenseItem = (props) => {
+  const clickHandler = () => {
+    console.log(`${props.title} Button was clicked.`);
+  };
+
+  return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{ props.title }</h2>
-        <div className="expense-item__price">${ props.amount }</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
+};
 
 export default ExpenseItem;
