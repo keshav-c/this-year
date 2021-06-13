52f59bfdd685432d622af6c4c43074a023f2ff50
Keshav Chakravarthy <keshav1985@gmail.com>
2021-06-13 14:20:41 +0530

    Add styles to ExpenseItem component and use variables for dynamic data input

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,17 @@
+import './ExpenseItem.css'
+
 function ExpenseItem() {
+  const expenseDate = new Date(2021, 6, 13);
+  const expenseTitle = "Health Insurance";
+  const expenseAmount = 500;
+
   return (
-    <div>
-      <div>June 13, 2021</div>
-      <div>
-        <h2>Health Insurance</h2>
+    <div className="expense-item">
+      <div>{ expenseDate.toISOString() }</div>
+      <div className="expense-item__description">
+        <h2>{ expenseTitle }</h2>
+        <div className="expense-item__price">${ expenseAmount }</div>
       </div>
-      <div>$500.00</div>
     </div>
   );
 }
