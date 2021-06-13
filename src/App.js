048f634383d1cef0fd0b4954bdb3ad826802b414
Keshav Chakravarthy <keshav1985@gmail.com>
2021-06-13 15:39:29 +0530

    Move the list of ExpenseItems to it's own component - Expenses.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import ExpenseItem from './components/ExpenseItem';
+import Expenses from "./components/Expenses";
 
 function App() {
-  const expenses = [
+  const items = [
     {
       id: "e1",
       date: new Date(2021, 6, 13),
@@ -27,31 +27,7 @@ function App() {
       amount: 875,
     },
   ];
-  return (
-    <div>
-      <h2>Let's get started!</h2>
-      <ExpenseItem
-        title={ expenses[0].title }
-        amount={ expenses[0].amount }
-        date={ expenses[0].date }
-      />
-      <ExpenseItem
-        title={ expenses[1].title }
-        amount={ expenses[1].amount }
-        date={ expenses[1].date }
-      />
-      <ExpenseItem
-        title={ expenses[2].title }
-        amount={ expenses[2].amount }
-        date={ expenses[2].date }
-      />
-      <ExpenseItem
-        title={ expenses[3].title }
-        amount={ expenses[3].amount }
-        date={ expenses[3].date }
-      />
-    </div>
-  );
+  return <Expenses items={items} />;
 }
 
 export default App;
