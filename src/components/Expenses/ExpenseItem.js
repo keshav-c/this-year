52d608bcfe975c7e61cf35590d744bcf3eddb955
Keshav Chakravarthy <keshav1985@gmail.com>
2021-06-13 17:32:27 +0530

    Make click handler update ExpenseItem title. Error: we still need to update component state.

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,15 +3,18 @@ import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 
 const ExpenseItem = (props) => {
+  let title = props.title;
+
   const clickHandler = () => {
-    console.log(`${props.title} Button was clicked.`);
+    title = 'New updated title!'; // we can't assign to props.title directly
+    console.log(title);
   };
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
