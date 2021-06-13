74abfa594df60d00139a6a00ad013d0805bafe71
Keshav Chakravarthy <keshav1985@gmail.com>
2021-06-13 19:23:37 +0530

    Use the event object passed to onChange handler function, to print title on every keystroke.

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,25 +1,31 @@
 import "./ExpenseForm.css";
 
-const ExpenseForm = () => (
-  <form>
-    <div className="new-expense__controls">
-      <div className="new-expense__control">
-        <label>Title</label>
-        <input type="text" />
+const ExpenseForm = () => {
+  const changeTitleHandler = (event) => {
+    console.log(event.target.value);
+  };
+
+  return (
+    <form>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label>Title</label>
+          <input type="text" onChange={changeTitleHandler} />
+        </div>
+        <div className="new-expense__control">
+          <label>Amount</label>
+          <input type="number" min="0.01" step="0.01" />
+        </div>
+        <div className="new-expense__control">
+          <label>Date</label>
+          <input type="date" min="2020-01-01" max="2025-12-31" />
+        </div>
       </div>
-      <div className="new-expense__control">
-        <label>Amount</label>
-        <input type="number" min="0.01" step="0.01" />
+      <div className="new-expense__actions">
+        <button type="submit">Add Expense</button>
       </div>
-      <div className="new-expense__control">
-        <label>Date</label>
-        <input type="date" min="2020-01-01" max="2025-12-31" />
-      </div>
-    </div>
-    <div className="new-expense__actions">
-      <button type="submit">Add Expense</button>
-    </div>
-  </form>
-);
+    </form>
+  );
+};
 
 export default ExpenseForm;
