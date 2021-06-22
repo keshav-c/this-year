f17b977cc71a12739de90994b3a2fe88376b0fd3
Keshav Chakravarthy <keshav1985@gmail.com>
2021-06-23 05:25:19 +0530

    Add a Filter child component to Expenses with a select element, and move state to parent on select.

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,30 +1,46 @@
-import "./Expenses.css";
-import Card from '../UI/Card';
-import ExpenseItem from './ExpenseItem';
+import { useState } from 'react';
 
-const Expenses = (props) => (
-    <Card className="expenses">
-      <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
-      />
-      <ExpenseItem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-      />
-      <ExpenseItem
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-      />
-      <ExpenseItem
-        title={props.items[3].title}
-        amount={props.items[3].amount}
-        date={props.items[3].date}
-      />
-    </Card>
+import "./Expenses.css";
+
+import Card from "../UI/Card";
+import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "./ExpensesFilter";
+
+const Expenses = (props) => {
+  const [year, setYear] = useState('');
+
+  const yearSelectHandler = (year) => {
+    setYear(year);
+    console.log(`Year ${year}`);
+  };
+
+  return (
+    <div>
+      <ExpensesFilter onYearSelect={yearSelectHandler}/>
+      <Card className="expenses">
+        <ExpenseItem
+          title={props.items[0].title}
+          amount={props.items[0].amount}
+          date={props.items[0].date}
+        />
+        <ExpenseItem
+          title={props.items[1].title}
+          amount={props.items[1].amount}
+          date={props.items[1].date}
+        />
+        <ExpenseItem
+          title={props.items[2].title}
+          amount={props.items[2].amount}
+          date={props.items[2].date}
+        />
+        <ExpenseItem
+          title={props.items[3].title}
+          amount={props.items[3].amount}
+          date={props.items[3].date}
+        />
+      </Card>
+    </div>
   );
+};
 
 export default Expenses;
