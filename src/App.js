c0d13a4b6c95205dc3c781b4e9af5cf73cc1004c
Keshav Chakravarthy <keshav1985@gmail.com>
2021-06-22 17:22:31 +0530

    Move form data from child (ExpenseForm) to parents (NewExpense and then App)

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
 const App = () => {
+  const addExpenseHandler = (expense) => {
+    console.log("In App.js");
+    console.log(expense);
+  };
   const expenses = [
     {
       id: "e1",
@@ -30,7 +34,7 @@ const App = () => {
   ];
   return (
     <div>
-      <NewExpense />
+      <NewExpense onAddExpense={addExpenseHandler}/>
       <Expenses items={expenses} />
     </div>
   );
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,10 +1,20 @@
 import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
 
-const NewExpense = () => (
-  <div className="new-expense">
-    <ExpenseForm />
-  </div>
-);
+const NewExpense = (props) => {
+  const saveExpenseHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString()
+    };
+    props.onAddExpense(expenseData);
+  };
+
+  return (
+    <div className="new-expense">
+      <ExpenseForm onSaveExpenseData={saveExpenseHandler} />
+    </div>
+  );
+};
 
 export default NewExpense;
