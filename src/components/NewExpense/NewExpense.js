b90d51959beed1c09b2378fc3a08b7901c4323f2
Keshav Chakravarthy <keshav1985@gmail.com>
2021-06-24 13:19:52 +0530

    Hide the user input form behind a button and allow users to cancel adding new expense

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,20 +1,40 @@
 import "./NewExpense.css";
+
+import { useState } from "react";
 import ExpenseForm from "./ExpenseForm";
 
 const NewExpense = (props) => {
+  const [showForm, setShowForm] = useState(false);
+
   const saveExpenseHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
-      id: Math.random().toString()
+      id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
   };
 
-  return (
-    <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseHandler} />
-    </div>
+  const displayFormHandler = () => {
+    setShowForm((showForm) => !showForm);
+  };
+
+  const showFormButton = (
+    <button type="button" onClick={displayFormHandler}>
+      Add New Expense
+    </button>
   );
+
+  let newExpenseContent = showFormButton;
+  if (showForm) {
+    newExpenseContent = (
+      <ExpenseForm
+        onSaveExpenseData={saveExpenseHandler}
+        onCancel={displayFormHandler}
+      />
+    );
+  }
+
+  return <div className="new-expense">{newExpenseContent}</div>;
 };
 
 export default NewExpense;
