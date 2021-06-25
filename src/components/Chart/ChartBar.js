a76f6f71759aac80ac93156235bdcbff934e0f5a
Keshav Chakravarthy <keshav1985@gmail.com>
2021-06-25 10:57:55 +0530

    Add markup for the ChartBar component

--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -1,3 +1,16 @@
-const ChartBar = (props) => {};
+import "./ChartBar.css";
+
+const ChartBar = (props) => {
+  let barFillHeight = "0%";
+  if (props.maxValue > 0) {
+    barFillHeight = Math.round((100 * props.value) / props.maxValue) + "%";
+  }
+  return (
+    <div className="chart-bar">
+      <div className="chart-bar__inner" style={{ height: barFillHeight }}></div>
+      <div className="chart-bar__label">{props.label}</div>
+    </div>
+  );
+};
 
 export default ChartBar;
